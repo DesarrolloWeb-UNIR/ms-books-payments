0ec84fae7f33a61488351686ef7585e25de98bb1
camilocamargo1226 <83561768+camilocamargo1226@users.noreply.github.com>
2026-02-26 13:49:05 -0500

    Update BookDTO.java

--- src/main/java/com/relato/msbookspayments/dto/BookDTO.java
+++ src/main/java/com/relato/msbookspayments/dto/BookDTO.java
@@ -1,7 +1,6 @@
 package com.relato.msbookspayments.dto;
 
 public class BookDTO {
-
     private Long id;
     private String title;
     private String author;
@@ -9,34 +8,16 @@ public class BookDTO {
     private String isbn;
     private Integer rating;
     private Boolean visible;
+    private Integer stock;
 
     public BookDTO() {}
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public Integer getRating() {
-        return rating;
-    }
-
-    public Boolean getVisible() {
-        return visible;
-    }
+    public Long getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getCategory() { return category; }
+    public String getIsbn() { return isbn; }
+    public Integer getRating() { return rating; }
+    public Boolean getVisible() { return visible; }
+    public Integer getStock() { return stock; }
 }
