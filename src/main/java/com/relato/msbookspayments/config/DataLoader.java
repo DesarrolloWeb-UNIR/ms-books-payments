ecbfc47972b65e3543209e3e1b26ab5f1202b1ff
camilocamargo1226 <83561768+camilocamargo1226@users.noreply.github.com>
2026-02-26 14:20:20 -0500

    Update DataLoader.java

--- src/main/java/com/relato/msbookspayments/config/DataLoader.java
+++ src/main/java/com/relato/msbookspayments/config/DataLoader.java
@@ -3,22 +3,27 @@ package com.relato.msbookspayments.config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.relato.msbookspayments.client.CatalogueClient;
+import com.relato.msbookspayments.dto.BookDTO;
 import com.relato.msbookspayments.entity.Purchase;
-import com.relato.msbookspayments.enums.PurchaseStatus;
 import com.relato.msbookspayments.repository.PurchaseRepository;
+
 @Configuration
 public class DataLoader {
+
     @Bean
-    CommandLineRunner runner(PurchaseRepository purchaseRepository) {
+    CommandLineRunner runner(PurchaseRepository purchaseRepository, CatalogueClient catalogueClient) {
         return args -> {
-            Purchase purchase = new Purchase(
-                    1L,
-                    "usuario@relato.com"
-            );
+            Purchase purchase = new Purchase(10L, "usuario@relato.com");
+            Purchase saved = purchaseRepository.save(purchase);
+            System.out.println("✅ Compra guardada con ID: " + saved.getId());
 
-            //purchaseRepository.save(purchase);
-
-            System.out.println("✅ Compra guardada con ID: " + purchase.getId());
+            try {
+                BookDTO book = catalogueClient.getBookById(10L);
+                System.out.println("✅ Libro encontrado: " + book.getTitle() + " - " + book.getAuthor());
+            } catch (Exception e) {
+                System.out.println("❌ Error al obtener libro: " + e.getMessage());
+            }
         };
     }
 }
